988e7dac8792a7b034074bfc3eaf5c97ac960b8f
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-05-14 18:58:54 -0600

    added place to store readme and added all creation and writing functionality

---
+++ Develop/dist/README.md
@@ -0,0 +1,30 @@
+
+  # fdj;ksajkl [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+  ## Description
+  *fdjska;
+  ## Table of Contents 
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Test](#tests)
+  6. [Questions](#questions)
+  ## Installation
+  fdjska
+  ## Usage
+  ## License 
+  MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  https://choosealicense.com/licenses/mit/
+  ## Contributing 
+  ## Tests
+  *Test Please!
+  ## Questions
+
+
+
+
+
+
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs")
+const inquirer = require("inquirer");
+ const generateMarkdown = require("./utils/generateMarkdown.js")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Enter the title of your project",
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your projects name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Enter a description of your project",
+            validate: aboutInput => {
+                if (aboutInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "install",
+            message: "Please enter the instructions on how to install your project",
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the installation instructions of your project');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "rawlist",
+            name: "license",
+            message: "Pick a license",
+            choices: [
+                "MIT", "Apache", "GNU"
+            ]
+        }
+    ]).then(project=> {
+        writeToFile('README', project)
+    })
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { 
+    const markdown = generateMarkdown(data)
+    console.log(markdown);
+    fs.writeFile('./dist/'+fileName+'.md', markdown, err => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        console.log('File created!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    questions()
+}
 
 // Function call to initialize app
 init();
